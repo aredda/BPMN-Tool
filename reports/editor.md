ad38180911e9c592f8fe8f2d80ea0ba0e54fac48
ibrahim-areda <aredda.ibrahim@gmail.com>
2020-06-26 17:30:32 +0100

    Finishing GUI Event

--- reports/editor.md
+++ reports/editor.md
@@ -4,3 +4,7 @@
   - [ ] Displaying a floating Entry to type something
 - [ ] Resizing container elements
 - [ ] Displaying a pop-up of an element's actions
+
+### 26th of June
+- [ ] Finishing the setup of GUI Events
+- [ ] Finishing the setup of GUI Gateways
--- views/prefabs/guievent.py
+++ views/prefabs/guievent.py
@@ -1,4 +1,5 @@
 from tkinter import Canvas
+from PIL import Image as Img, ImageTk as ImgTk
 from resources.colors import *
 from views.prefabs.abstract.guilinkable import GUILinkable
 from models.bpmn.enums.eventtype import EventType
@@ -7,6 +8,7 @@ from models.bpmn.enums.eventdefinition import EventDefinition
 class GUIEvent(GUILinkable):
     
     PERIMETER = 60
+    ICON_SIZE = 40
 
     def __init__(self, **args):
         GUILinkable.__init__(self, **args)
@@ -14,17 +16,33 @@ class GUIEvent(GUILinkable):
     def draw_at(self, x, y):
         GUILinkable.draw_at(self, x, y)
         # Extract info
-        eventtype = EventType.IntermediateThrow
+        eventtype = EventType.End
+        eventdefinition = EventDefinition.Terminate
         # cast
         cnv: Canvas = self.canvas
         # figure out the border width of the circle
-        borderwidth = (4 if eventtype == EventType.End else 2)
+        borderwidth = (6 if eventtype == EventType.End else 2)
         # draw borders
         self.id.append (cnv.create_oval(x, y, x + self.PERIMETER, y + self.PERIMETER, fill=cnv['bg'], outline=black, width=borderwidth))
         # draw inner border
         if eventtype in [EventType.IntermediateCatch, EventType.IntermediateThrow]:
             self.id.append (cnv.create_oval(x + 4, y + 4, x + self.PERIMETER - 4, y + self.PERIMETER - 4, fill=cnv['bg'], outline=black, width=borderwidth))
+        # draw definition icon
+        if eventdefinition != EventDefinition.Default:
+            folder = 'resources/icons/notation/'
+            pure_name = str(eventdefinition).split('.')[1].lower()
+            # correct names
+            if eventdefinition == EventDefinition.Message:
+                pure_name = 'receive' if eventtype not in [EventType.Start, EventType.IntermediateThrow] else 'send'
+            # display icon
+            image = Img.open(folder + pure_name + '.png').resize((self.ICON_SIZE, self.ICON_SIZE))
+            self.def_icon = ImgTk.PhotoImage(image)
+            # add a black overlay if it's end/throw event
+            if eventtype in [EventType.End, EventType.IntermediateThrow]:
+                overlaid_img = Img.new('RGBA', image.size, color=black)
+                overlaid_img.putalpha(image.getchannel('A'))
+                self.def_icon = ImgTk.PhotoImage(overlaid_img)
+            self.id.append (cnv.create_image(self.PERIMETER/2, self.PERIMETER/2, image=self.def_icon))
             
-
     def move(self, x, y):
         super().move(x - (self.PERIMETER/2), y - (self.PERIMETER/2))
